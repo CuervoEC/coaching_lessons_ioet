bc02296b11a5c91d892253b84eaf458a2d0f7667
Francisco Navarro <francisco.navarro@ioet.com>
2021-12-20 13:07:59 -0500

    Added setup and teardown example

---
+++ tests/fizz_buzz_test_setup_teardown.py
@@ -0,0 +1,53 @@
+import sys
+import unittest
+
+from fizz_buzz.fizz_buzz import fizz_buzz
+
+
+class FizzBuzzTestClass(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super(FizzBuzzTestClass, self).setUp()
+        self.mock_data = [1, 2, 3, 4, 5]
+
+    def test_should_show_number_if_is_not_multiple_of_three(self):
+
+        # Arrange
+        number_input = self.mock_data[0]
+
+        # Act
+        result = fizz_buzz(number_input)
+
+        # Assert
+        self.assertEqual(number_input, result)
+
+        # TearDown
+
+    def test_should_show_fizz_with_multiple_of_three(self):
+
+        # Arrange
+        number_input = self.mock_data[2]
+
+        # Act
+        result = fizz_buzz(number_input)
+
+        # Assert
+        self.assertEqual('fizz', result)
+
+    def test_should_show_buzz_with_multiple_of_three(self):
+        # Arrange
+        number_input = self.mock_data[4]
+
+        # Act
+        result = fizz_buzz(number_input)
+
+        # Assert
+        self.assertEqual('buzz', result)
+
+    def tearDown(self) -> None:
+        super(FizzBuzzTestClass, self).setUp()
+        self.mock_data = []
+
+
+if __name__ == '__main__':
+    unittest.main()
