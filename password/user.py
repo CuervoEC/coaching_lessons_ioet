4241907046e175f498dceeedd9d975efb496df4f
Francisco Navarro <francisco.navarro@ioet.com>
2021-12-21 09:23:23 -0500

    Added hashing to user password

--- password/user.py
+++ password/user.py
@@ -1,5 +1,6 @@
 from typing import List
 from dataclasses import dataclass
+from hashlib import blake2b
 
 
 @dataclass
@@ -11,3 +12,14 @@ class User(object):
 
     def get_username(self):
         return self.user_data[0]
+
+    def get_password(self):
+        b_password = bytes(str(self.user_data[1]), 'utf8')
+        hashed_password = blake2b(b_password).hexdigest()
+        return hashed_password
+
+    def check_valid_credentials(self, credentials: List):
+        hashed_credential = blake2b(bytes(str(credentials[1]), 'utf8')).hexdigest()
+        if self.get_username() == credentials[0] and self.get_password() == hashed_credential:
+            return True
+        return False
--- tests/test_password_class.py
+++ tests/test_password_class.py
@@ -1,4 +1,5 @@
 import unittest
+from hashlib import blake2b
 
 from password import User
 
@@ -19,6 +20,18 @@ class TestPassword(unittest.TestCase):
         user = User(['Elisa'])
         username = user.get_username()
         self.assertEqual(username, 'Elisa')
+
+    def test_get_hash_password(self):
+        user = User(['', 55555])
+        user_password = user.get_password()
+        self.assertEqual(user_password, blake2b(b'55555').hexdigest())
+
+    def test_check_valid_user_credentials(self):
+        user = User(['Jose', 12345])
+        valid_credential = user.check_valid_credentials(['Jose', 12345])
+        invalid_credential = user.check_valid_credentials(['Maria', 11111])
+        self.assertTrue(valid_credential)
+        self.assertFalse(invalid_credential)
 
 
 if __name__ == '__main__':
