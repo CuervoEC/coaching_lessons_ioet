16c64a70f0b0a44f8e40445fd5745ca62af8c0ba
Francisco Navarro <francisco.navarro@ioet.com>
2022-05-18 11:23:17 -0500

    Mocker fix / requirements update / gitignore update

--- birthday_greetings/main.py
+++ birthday_greetings/main.py
@@ -1,11 +1,15 @@
 import csv
+import os
+
 
 def reader_csv_file():
-    with open('birthday_data.csv', newline='') as csvfile:
+    csv_dir = os.path.dirname(__file__)
+    rel_path = "birthday_data.csv"
+    abs_file_path = os.path.join(csv_dir, rel_path)
+    with open(abs_file_path, newline='') as csvfile:
         birthday_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         csv_list = [row for row in birthday_reader]
         return csv_list[0]
-
 
 
 print(reader_csv_file())
--- tests/birthday_greetings/test_a.py
+++ tests/birthday_greetings/test_a.py
@@ -1,8 +1,8 @@
 from birthday_greetings.main import reader_csv_file
 
 
-def test_open_csv_file():
-    mocker.patch('builtins.open', )
-    first_line = reader_csv_file()
-    assert ['last_name', 'first_name', 'date_of_birth', 'email'] == first_line
-
+def test_open_csv_file(mocker):
+    csv_content = ['last_name', 'first_name', 'date_of_birth', 'email']
+    mocker.patch('birthday_greetings.main.reader_csv_file', return_value=csv_content)
+    csv_reader = reader_csv_file()
+    assert csv_reader == csv_content
