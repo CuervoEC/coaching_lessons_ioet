75e9c7db6cfb02fae9c239438f7dc8ecab80c95f
Luis Moyón <amoyon@hotmail.com>
2022-06-20 14:10:57 -0500

    Coaching session 14/06/22

--- birthday_greetings/main.py
+++ birthday_greetings/main.py
@@ -9,14 +9,24 @@ def reader_csv_file():
     with open(abs_file_path, newline='') as csvfile:
         birthday_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         csv_list = [row for row in birthday_reader]
-        headers = csv_list[0]
+        header = csv_list[0]
         body = csv_list[1:]
-        return headers, body
+        return header, body
 
 
-def send_email(name):
+def build_email_message(name):
     email = f'happy birthday dear {name}'
     return email
 
+def get_full_name(first_name, last_name):
+    return f'{first_name} {last_name}'
 
-print(reader_csv_file())
+def send_email():
+    header, body = reader_csv_file()
+    first_name_index = header.index('first_name')
+    last_name_index = header.index('last_name')
+    for line in body:
+        full_name = get_full_name(first_name = line[first_name_index], last_name = line[last_name_index])
+        print(build_email_message(full_name))
+
+send_email()
--- tests/birthday_greetings/test_a.py
+++ tests/birthday_greetings/test_a.py
@@ -1,4 +1,7 @@
-from birthday_greetings.main import reader_csv_file, send_email
+from unittest.mock import Mock
+from pytest_mock import mocker
+from birthday_greetings.main import get_full_name, reader_csv_file, build_email_message, send_email
+
 
 CSV_FILE_CONTENT = (
     ['last_name', 'first_name', 'date_of_birth', 'email'],
@@ -14,13 +17,45 @@ def test_open_csv_file():
     assert result_data == CSV_FILE_CONTENT
 
 
-def test_send_email():
+def test_build_email_message():
     # Setup
     mock_fullname = 'luis mMoyon'
     expected_result = f'happy birthday dear {mock_fullname}'
     # Act
 
-    result = send_email(mock_fullname)
+    result = build_email_message(mock_fullname)
     # Assert
     assert result == expected_result
     # Clean Up
+
+def test_get_full_name():
+    mock_first_name = 'luis'
+    mock_last_name = 'moyón'
+    expected_result = 'luis moyón'
+
+    result = get_full_name(mock_first_name, mock_last_name)
+
+    assert result == expected_result
+
+def test_send_email(mocker):
+    import birthday_greetings.main as birthday 
+    mocked_reader_csv_file = mocker.patch('birthday_greetings.main.reader_csv_file')
+    mocked_reader_csv_file.side_effect = Mock(return_value = CSV_FILE_CONTENT)
+    spy_get_full_name = mocker.spy(birthday, 'get_full_name')
+    spy_build_email_message = mocker.spy(birthday, 'build_email_message')
+    get_full_name_calls = [
+        mocker.call(first_name='John', last_name='Doe'), 
+        mocker.call(first_name='Mary', last_name='Ann')
+    ]
+    build_email_message_calls = [
+        mocker.call('John Doe'), 
+        mocker.call('Mary Ann')
+    ]
+
+    send_email()
+
+    mocked_reader_csv_file.assert_called_once()
+    assert spy_get_full_name.call_count == 2
+    assert spy_build_email_message.call_count == 2
+    spy_get_full_name.assert_has_calls(get_full_name_calls)
+    spy_build_email_message.assert_has_calls(build_email_message_calls)
