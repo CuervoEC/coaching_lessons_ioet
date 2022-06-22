be82d0d74ba07be674bd6768c384b94d047d5ab2
Francisco Navarro <francisco.navarro@ioet.com>
2022-06-22 10:33:31 -0500

    Coaching session 21/06/22

--- birthday_greetings/main.py
+++ birthday_greetings/main.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 import os
 
 
@@ -18,15 +19,23 @@ def build_email_message(name):
     email = f'happy birthday dear {name}'
     return email
 
+
 def get_full_name(first_name, last_name):
     return f'{first_name} {last_name}'
+
 
 def send_email():
     header, body = reader_csv_file()
     first_name_index = header.index('first_name')
     last_name_index = header.index('last_name')
     for line in body:
-        full_name = get_full_name(first_name = line[first_name_index], last_name = line[last_name_index])
-        print(build_email_message(full_name))
+        full_name = get_full_name(first_name=line[first_name_index], last_name=line[last_name_index])
+        build_email_message(full_name)
 
-send_email()
+
+def check_next_birthday(birthday_str):
+    actual_date = datetime.datetime.now()
+    birthday_date = datetime.datetime.strptime(birthday_str, '%Y/%m/%d')
+    if birthday_date.month - actual_date.month == 0 and birthday_date.day - actual_date.day == 1:
+        return True
+    return False
--- tests/birthday_greetings/test_a.py
+++ tests/birthday_greetings/test_a.py
@@ -1,46 +1,56 @@
-from unittest.mock import Mock
-from pytest_mock import mocker
-from birthday_greetings.main import get_full_name, reader_csv_file, build_email_message, send_email
+import datetime
+
+import birthday_greetings.main as birthday
 
 
 CSV_FILE_CONTENT = (
     ['last_name', 'first_name', 'date_of_birth', 'email'],
-    [['Doe', 'John', '1982/10/08', 'john.doe@foobar.com'],
-     ['Ann', 'Mary', '1975/09/11', 'mary.ann@foobar.com']]
+    [
+        ['Doe', 'John', '1982/10/08', 'john.doe@foobar.com'],
+        ['Ann', 'Mary', '1975/09/11', 'mary.ann@foobar.com'],
+        ['Luis', 'Lopez', '1999/10/08', 'luis.lopez@foobar.com']
+    ]
 )
+
+
+# @pytest.fixture
+# def reader_csv_file_fixture(mocker):
+#     mocked_reader_csv_file = mocker.patch('birthday_greetings.main.reader_csv_file')
+#     mocked_reader_csv_file.return_value = CSV_FILE_CONTENT
 
 
 def test_open_csv_file():
 
-    result_data = reader_csv_file()
+    result_data = birthday.reader_csv_file()
 
     assert result_data == CSV_FILE_CONTENT
 
 
 def test_build_email_message():
     # Setup
-    mock_fullname = 'luis mMoyon'
+    mock_fullname = 'Luis Moyon'
     expected_result = f'happy birthday dear {mock_fullname}'
     # Act
 
-    result = build_email_message(mock_fullname)
+    result = birthday.build_email_message(mock_fullname)
     # Assert
     assert result == expected_result
     # Clean Up
 
-def test_get_full_name():
-    mock_first_name = 'luis'
-    mock_last_name = 'moyón'
-    expected_result = 'luis moyón'
 
-    result = get_full_name(mock_first_name, mock_last_name)
+def test_get_full_name():
+    mock_first_name = 'Luis'
+    mock_last_name = 'Moyon'
+    expected_result = 'Luis Moyon'
+
+    result = birthday.get_full_name(mock_first_name, mock_last_name)
 
     assert result == expected_result
 
+
 def test_send_email(mocker):
-    import birthday_greetings.main as birthday 
     mocked_reader_csv_file = mocker.patch('birthday_greetings.main.reader_csv_file')
-    mocked_reader_csv_file.side_effect = Mock(return_value = CSV_FILE_CONTENT)
+    mocked_reader_csv_file.return_value = CSV_FILE_CONTENT
     spy_get_full_name = mocker.spy(birthday, 'get_full_name')
     spy_build_email_message = mocker.spy(birthday, 'build_email_message')
     get_full_name_calls = [
@@ -52,10 +62,34 @@ def test_send_email(mocker):
         mocker.call('Mary Ann')
     ]
 
-    send_email()
+    birthday.send_email()
 
     mocked_reader_csv_file.assert_called_once()
-    assert spy_get_full_name.call_count == 2
-    assert spy_build_email_message.call_count == 2
+    assert spy_get_full_name.call_count == len(CSV_FILE_CONTENT[1])
+    assert spy_build_email_message.call_count == len(CSV_FILE_CONTENT[1])
     spy_get_full_name.assert_has_calls(get_full_name_calls)
     spy_build_email_message.assert_has_calls(build_email_message_calls)
+
+
+def test_check_next_day_is_a_birthday(mocker):
+
+    day_str = '1999/10/10'
+    birthday_str = '1999/10/09'
+    mock_actual_day = mocker.patch('datetime.datetime.now')
+    mock_actual_day.return_value = datetime.datetime.strptime(day_str, '%Y/%m/%d')
+
+    result = birthday.check_next_birthday(birthday_str)
+
+    assert result is True
+
+
+def test_check_next_day_is_not_a_birthday(mocker):
+
+    day_str = '1999/09/10'
+    birthday_str = '1999/10/09'
+    mock_actual_day = mocker.patch('datetime.datetime.now')
+    mock_actual_day.return_value = datetime.datetime.strptime(day_str, '%Y/%m/%d')
+
+    result = birthday.check_next_birthday(birthday_str)
+
+    assert result is False
